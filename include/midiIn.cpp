b57db35d2b47eb544936deebb7c91c17ae7e9d8f
Myungin Lee <benedic.lee@gmail.com>
2022-03-24 14:52:59 -0700

    MIDI input test

---
+++ include/midiIn.cpp
@@ -0,0 +1,84 @@
+// MIDI input Receiver
+#include <stdio.h>
+
+#include "al/app/al_App.hpp"
+#include "al/io/al_MIDI.hpp"
+using namespace al;
+
+class MIDI : public MIDIMessageHandler
+{
+public:
+    RtMidiIn midiIn;
+    void midiInit()
+    {
+        // Check for connected MIDI devices
+        if (midiIn.getPortCount() > 0)
+        {
+            // Bind ourself to the RtMidiIn object, to have the onMidiMessage()
+            // callback called whenever a MIDI message is received
+            MIDIMessageHandler::bindTo(midiIn);
+
+            // Open the last device found
+            unsigned int port = midiIn.getPortCount() - 1;
+            midiIn.openPort(port);
+            printf("Opened port to %s\n", midiIn.getPortName(port).c_str());
+        }
+        else
+        {
+            printf("Error: No MIDI devices found.\n");
+        }
+    }
+    // This gets called whenever a MIDI message is received on the port
+    void onMIDIMessage(const MIDIMessage &m)
+    {
+        printf("%s: ", MIDIByte::messageTypeString(m.status()));
+
+        // Here we demonstrate how to parse common channel messages
+        switch (m.type())
+        {
+        case MIDIByte::NOTE_ON:
+            printf("Note %u, Vel %f", m.noteNumber(), m.velocity());
+            break;
+
+        case MIDIByte::NOTE_OFF:
+            printf("Note %u, Vel %f", m.noteNumber(), m.velocity());
+            break;
+
+        case MIDIByte::PITCH_BEND:
+            printf("Value %f", m.pitchBend());
+            break;
+
+        // Control messages need to be parsed again...
+        case MIDIByte::CONTROL_CHANGE:
+            printf("%s ", MIDIByte::controlNumberString(m.controlNumber()));
+            switch (m.controlNumber())
+            {
+            case MIDIByte::MODULATION:
+                printf("%f", m.controlValue());
+                break;
+
+            case MIDIByte::EXPRESSION:
+                printf("%f", m.controlValue());
+                break;
+            }
+            break;
+        default:;
+        }
+
+        // If it's a channel message, print out channel number
+        if (m.isChannelMessage())
+        {
+            printf(" (MIDI chan %u)", m.channel() + 1);
+        }
+
+        printf("\n");
+
+        // Print the raw byte values and time stamp
+        printf("\tBytes = ");
+        for (unsigned i = 0; i < 3; ++i)
+        {
+            printf("%3u ", (int)m.bytes[i]);
+        }
+        printf(", time = %g\n", m.timeStamp());
+    }
+};

567e964a18e438d7acb6c356e63679652adb6088
ben <benedic.lee@gmail.com>
2022-11-14 02:25:05 -0800

    pluck reverb

---
+++ 09_pluck_reverb.cpp
@@ -0,0 +1,369 @@
+// MUS109IA & MAT276IA.
+// Spring 2022
+// Course Instrument 09. Plucked String synthesis-Visual (Mesh & Spectrum)
+// Press '[' or ']' to turn on & off GUI
+// Able to play with MIDI device
+// Myungin Lee
+#include <cstdio> // for printing to stdout
+
+#include "Gamma/Analysis.h"
+#include "Gamma/Effects.h"
+#include "Gamma/Envelope.h"
+#include "Gamma/Gamma.h"
+#include "Gamma/Oscillator.h"
+#include "Gamma/Types.h"
+#include "Gamma/DFT.h"
+
+#include "al/app/al_App.hpp"
+#include "al/graphics/al_Shapes.hpp"
+#include "al/scene/al_PolySynth.hpp"
+#include "al/scene/al_SynthSequencer.hpp"
+#include "al/ui/al_ControlGUI.hpp"
+#include "al/ui/al_Parameter.hpp"
+#include "al/io/al_MIDI.hpp"
+#include "al/math/al_Random.hpp"
+#include "al/sound/al_Reverb.hpp"
+
+// using namespace gam;
+using namespace al;
+using namespace std;
+#define FFT_SIZE 4048
+
+class PluckedString : public SynthVoice
+{
+public:
+    float mAmp;
+    float mDur;
+    float mPanRise;
+    gam::Pan<> mPan;
+    gam::NoiseWhite<> noise;
+    gam::Decay<> env;
+    gam::MovingAvg<> fil{2};
+    gam::Delay<float, gam::ipl::Trunc> delay;
+    gam::ADSR<> mAmpEnv;
+    gam::EnvFollow<> mEnvFollow;
+    gam::Env<2> mPanEnv;
+    gam::STFT stft = gam::STFT(FFT_SIZE, FFT_SIZE / 4, 0, gam::HANN, gam::MAG_FREQ);
+    Reverb<float> reverb;
+// This time, let's use spectrograms for each notes as the visual components.
+    Mesh mSpectrogram;
+    vector<float> spectrum;
+    double a = 0;
+    double b = 0;
+    double timepose = 10;
+    // Additional members
+    Mesh mMesh;
+    
+    virtual void init() override
+    {
+        // Declare the size of the spectrum
+        spectrum.resize(FFT_SIZE / 2 + 1);
+        // mSpectrogram.primitive(Mesh::POINTS);
+        mSpectrogram.primitive(Mesh::LINE_STRIP);
+        mAmpEnv.levels(0, 1, 1, 0);
+        mPanEnv.curve(4);
+        env.decay(0.1);
+        delay.maxDelay(1. / 27.5);
+        delay.delay(1. / 440.0);
+        reverb.bandwidth(0.6f); // Low-pass amount on input, in [0,1]
+        reverb.damping(0.5f);   // High-frequency damping, in [0,1]
+        reverb.decay(0.6f);     // Tail decay factor, in [0,1]
+
+        // Diffusion amounts
+        // Values near 0.7 are recommended. Moving further away from 0.7 will lead
+        // to more distinct echoes.
+        reverb.diffusion(0.76, 0.666, 0.707, 0.571);
+
+        addDisc(mMesh, 1.0, 30);
+        createInternalTriggerParameter("amplitude", 0.1, 0.0, 1.0);
+        createInternalTriggerParameter("frequency", 60, 20, 5000);
+        createInternalTriggerParameter("attackTime", 0.001, 0.001, 1.0);
+        createInternalTriggerParameter("releaseTime", 3.0, 0.1, 10.0);
+        createInternalTriggerParameter("sustain", 0.7, 0.0, 1.0);
+        createInternalTriggerParameter("Pan1", 0.0, -1.0, 1.0);
+        createInternalTriggerParameter("Pan2", 0.0, -1.0, 1.0);
+        createInternalTriggerParameter("PanRise", 0.0, 0, 3.0); // range check
+        createInternalTriggerParameter("reverberation", 0.3, 0., 1);
+    }
+
+    //    void reset(){ env.reset(); }
+
+    float operator()()
+    {
+        return (*this)(noise() * env());
+    }
+    float operator()(float in)
+    {
+        return delay(
+            fil(delay() + in));
+    }
+
+    virtual void onProcess(AudioIOData &io) override
+    {
+
+        while (io())
+        {
+            mPan.pos(mPanEnv());
+            float s1 = (*this)() * mAmpEnv() * mAmp;
+            float s2;
+
+            // Compute two wet channels of reverberation
+            float wet1, wet2;
+            reverb(s1, wet1, wet2);
+
+            mEnvFollow(wet1);
+            mPan(wet1, wet1, wet2);
+
+            io.out(0) += wet1;
+            io.out(1) += wet2;
+
+            // STFT for each notes
+            if (stft(wet1+wet2))
+            { // Loop through all the frequency bins
+                for (unsigned k = 0; k < stft.numBins(); ++k)
+                {
+                    // Here we simply scale the complex sample
+                    spectrum[k] = tanh(pow(stft.bin(k).real(), 1.3));
+                }
+            }
+        }
+        if (mAmpEnv.done() && (mEnvFollow.value() < 0.001))
+            free();
+    }
+
+    virtual void onProcess(Graphics &g) override
+    {
+        float frequency = getInternalParameterValue("frequency");
+        float amplitude = getInternalParameterValue("amplitude");
+        a += 3.29;
+        b += 2.23;
+        timepose -= 0.1;
+
+        mSpectrogram.reset();
+        // mSpectrogram.primitive(Mesh::LINE_STRIP);
+
+        for (int i = 0; i < FFT_SIZE / 2; i++)
+        {
+            mSpectrogram.color(HSV(al::rnd::uniform(spectrum[i] * 700) 
+            , 0.9+ al::rnd::uniform(200*spectrum[i])
+            , 10000*spectrum[i] ));
+            mSpectrogram.vertex(i+spectrum[i], 0.5*tanh(spectrum[i]), sin(i*0.01));
+        }
+        g.meshColor(); // Use the color in the mesh
+        g.pushMatrix();
+        g.translate(0, 0, -8);
+        g.rotate(a-timepose, Vec3f(1, 0, 1));
+        g.rotate(a-timepose, Vec3f(0, 1, 0));
+        g.rotate(b-timepose, Vec3f(0, 1, 1));
+        g.scale(10.0 / FFT_SIZE, 500, 1.0);
+        g.draw(mSpectrogram);
+        g.popMatrix();
+    }
+
+    virtual void onTriggerOn() override
+    {
+        mAmpEnv.reset();
+        timepose = 10;
+        updateFromParameters();
+        env.reset();
+        delay.zero();
+        mPanEnv.reset();
+    }
+
+    virtual void onTriggerOff() override
+    {
+        mAmpEnv.triggerRelease();
+    }
+
+    void updateFromParameters()
+    {
+        mPanEnv.levels(getInternalParameterValue("Pan1"),
+                       getInternalParameterValue("Pan2"),
+                       getInternalParameterValue("Pan1"));
+        mPanRise = getInternalParameterValue("PanRise");
+        delay.freq(getInternalParameterValue("frequency"));
+        mAmp = getInternalParameterValue("amplitude");
+        mAmpEnv.levels()[1] = 1.0;
+        mAmpEnv.levels()[2] = getInternalParameterValue("sustain");
+        mAmpEnv.lengths()[0] = getInternalParameterValue("attackTime");
+        mAmpEnv.lengths()[3] = getInternalParameterValue("releaseTime");
+        mPanEnv.lengths()[0] = mPanRise;
+        mPanEnv.lengths()[1] = mPanRise;
+        reverb.decay(getInternalParameterValue("reverberation"));
+    }
+};
+
+class MyApp : public App, public MIDIMessageHandler
+{
+public:
+    SynthGUIManager<PluckedString> synthManager{"plunk"};
+    //    ParameterMIDI parameterMIDI;
+    RtMidiIn midiIn; // MIDI input carrier
+    Mesh mSpectrogram;
+    vector<float> spectrum;
+    bool showGUI = true;
+    bool showSpectro = true;
+    bool navi = false;
+    gam::STFT stft = gam::STFT(FFT_SIZE, FFT_SIZE / 4, 0, gam::HANN, gam::MAG_FREQ);
+
+    virtual void onInit() override
+    {
+        imguiInit();
+        navControl().active(false); // Disable navigation via keyboard, since we
+                                    // will be using keyboard for note triggering
+        // Set sampling rate for Gamma objects from app's audio
+        gam::sampleRate(audioIO().framesPerSecond());
+        // Check for connected MIDI devices
+        if (midiIn.getPortCount() > 0)
+        {
+            // Bind ourself to the RtMidiIn object, to have the onMidiMessage()
+            // callback called whenever a MIDI message is received
+            MIDIMessageHandler::bindTo(midiIn);
+
+            // Open the last device found
+            unsigned int port = midiIn.getPortCount() - 1;
+            midiIn.openPort(port);
+            printf("Opened port to %s\n", midiIn.getPortName(port).c_str());
+        }
+        else
+        {
+            printf("Error: No MIDI devices found.\n");
+        }
+        // Declare the size of the spectrum
+        spectrum.resize(FFT_SIZE / 2 + 1);
+    }
+
+    void onCreate() override
+    {
+        // Play example sequence. Comment this line to start from scratch
+        //    synthManager.synthSequencer().playSequence("synth8.synthSequence");
+        synthManager.synthRecorder().verbose(true);
+    }
+
+    void onSound(AudioIOData &io) override
+    {
+        synthManager.render(io); // Render audio
+        // STFT
+        while (io())
+        {
+            if (stft(io.out(0)))
+            { // Loop through all the frequency bins
+                for (unsigned k = 0; k < stft.numBins(); ++k)
+                {
+                    // Here we simply scale the complex sample
+                    spectrum[k] = tanh(pow(stft.bin(k).real(), 1.3));
+                    // spectrum[k] = stft.bin(k).real();
+                }
+            }
+        }        
+    }
+
+    void onAnimate(double dt) override
+    {
+        navControl().active(navi); // Disable navigation via keyboard, since we
+        imguiBeginFrame();
+        synthManager.drawSynthControlPanel();
+        imguiEndFrame();
+    }
+
+    void onDraw(Graphics &g) override
+    {
+        g.clear();
+        synthManager.render(g);
+        // GUI is drawn here
+        if (showGUI)
+        {
+            imguiDraw();
+        }
+    }
+  // This gets called whenever a MIDI message is received on the port
+  void onMIDIMessage(const MIDIMessage &m)
+  {
+    switch (m.type())
+    {
+    case MIDIByte::NOTE_ON:
+    {
+      int midiNote = m.noteNumber();
+      if (midiNote > 0 && m.velocity() > 0.001)
+      {
+        synthManager.voice()->setInternalParameterValue(
+            "frequency", ::pow(2.f, (midiNote - 69.f) / 12.f) * 432.f);
+        synthManager.voice()->setInternalParameterValue(
+            "attackTime", 0.01 / m.velocity());
+        synthManager.triggerOn(midiNote);
+      }
+      else
+      {
+        synthManager.triggerOff(midiNote);
+      }
+      break;
+    }
+    case MIDIByte::NOTE_OFF:
+    {
+      int midiNote = m.noteNumber();
+      synthManager.triggerOff(midiNote);
+      break;
+    }
+    default:;
+    }
+  }
+    bool onKeyDown(Keyboard const &k) override
+    {
+        if (ParameterGUI::usingKeyboard())
+        { // Ignore keys if GUI is using them
+            return true;
+        }
+        if (k.shift())
+        {
+            // If shift pressed then keyboard sets preset
+            int presetNumber = asciiToIndex(k.key());
+            synthManager.recallPreset(presetNumber);
+        }
+        else
+        {
+            // Otherwise trigger note for polyphonic synth
+            int midiNote = asciiToMIDI(k.key());
+            if (midiNote > 0)
+            {
+                synthManager.voice()->setInternalParameterValue(
+                    "frequency", ::pow(2.f, (midiNote - 69.f) / 12.f) * 432.f);
+                synthManager.triggerOn(midiNote);
+            }
+        }
+        switch (k.key())
+        {
+        case ']':
+        showGUI = !showGUI;
+        break;
+        case '[':
+        showSpectro = !showSpectro;
+        break;
+        case '=':
+        navi = !navi;
+        break;
+        }
+        return true;
+    }
+
+    bool onKeyUp(Keyboard const &k) override
+    {
+        int midiNote = asciiToMIDI(k.key());
+        if (midiNote > 0)
+        {
+            synthManager.triggerOff(midiNote);
+        }
+        return true;
+    }
+
+    void onExit() override { imguiShutdown(); }
+};
+
+int main()
+{
+    MyApp app;
+
+    // Set up audio
+    app.configureAudio(48000., 1024, 2, 0);
+
+    app.start();
+}
